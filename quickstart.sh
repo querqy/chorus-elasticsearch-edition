de43443be9c1492974e5e2ed93d6e6a5306beade
jzonthemtn <jzemerick@opensourceconnections.com>
2024-02-14 09:58:44 -0500

    Simplifying the quick start.

--- quickstart.sh
+++ quickstart.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # This script starts up Chorus and runs through the basic setup tasks.
-
-set -e
 
 # Ansi color code variables
 ERROR='\033[0;31m[QUICKSTART] '
@@ -42,17 +40,12 @@ do
       echo -e "Use the option --with-offline-lab | -lab to include Quepid and RRE services in Chorus."
       echo -e "Use the option --shutdown | -s to shutdown and remove the Docker containers and data."
       echo -e "Use the option --stop to stop the Docker containers."
-      echo -e "Use the option --online-deployment | -online to update configuration to run on chorus-es-edition.dev.o19s.com environment."
 			exit
 			;;
     --with-offline-lab | -lab)
 			offline_lab=true
       echo -e "${MAJOR}Running Chorus with offline lab environment enabled\n${RESET}"
 			;;
-    --online-deployment | -online)
-  		local_deploy=false
-      echo -e "${MAJOR}Configuring Chorus for chorus-es-edition.dev.o19s.com environment${RESET}"
-  		;;
     --shutdown | -s)
 			shutdown=true
       echo -e "${MAJOR}Shutting down Chorus\n${RESET}"
@@ -68,19 +61,11 @@ done
 services="opensearch opensearch-dashboards chorus-ui smui"
 
 if $offline_lab; then
-  services="${services} quepid rre keycloak"
-fi
-
-if ! $local_deploy; then
-  echo -e "${MAJOR}Updating configuration files for online deploy${RESET}"
-  sed -i.bu 's/localhost:3000/chorus-es-edition.dev.o19s.com:3000/g'  ./keycloak/realm-config/chorus-realm.json
-  sed -i.bu 's/keycloak:9080/chorus-es-edition.dev.o19s.com:9080/g'  ./keycloak/wait-for-keycloak.sh
-  sed -i.bu 's/keycloak:9080/chorus-es-edition.dev.o19s.com:9080/g'  ./docker-compose.yml
-  sed -i.bu 's/localhost:9200/chorus-es-edition.dev.o19s.com:9200/g'  ./reactivesearch/src/App.js
+  services="${services} quepid rre"
 fi
 
 if $stop; then
-  services="${services} quepid rre keycloak"
+  services="${services} quepid rre"
   docker compose stop ${services}
   exit
 fi
@@ -95,32 +80,9 @@ docker compose up -d --build ${services}
 echo -e "${MAJOR}Waiting for OpenSearch to start up and be online.${RESET}"
 ./opensearch/wait-for-os.sh # Wait for OpenSearch to be online
 
-# echo -e "${MAJOR}Setting up an admin user to explore OpenSearch and a role for anonymous access to run RRE offline tests.\n${RESET}"
-# curl -X POST "localhost:9200/_security/user/chorus_admin?pretty" -H 'Content-Type: application/json' -d'
-# {
-#   "password" : "password",
-#   "roles" : ["superuser"]
-# }
-# '
-
-# curl -X POST "localhost:9200/_security/role/anonymous_user" -H 'Content-Type: application/json' -d'
-# {
-#   "run_as": [ ],
-#   "cluster": [ ],
-#   "indices": [
-#     {
-#       "names": [ "ecommerce" ],
-#       "privileges": [ "read" ]
-#     }
-#   ]
-# }
-# '
-
 echo -e "${MAJOR}Creating ecommerce index, defining its mapping & settings\n${RESET}"
 curl -s -X PUT "localhost:9200/ecommerce/" -H 'Content-Type: application/json' --data-binary @./opensearch/schema.json
-
-curl -s -X PUT "http://localhost:9200/ecommerce/_settings"  -H 'Content-Type: application/json' -d '{"index.mapping.total_fields.limit": 10000}'
-
+curl -s -X PUT "localhost:9200/ecommerce/_settings"  -H 'Content-Type: application/json' -d '{"index.mapping.total_fields.limit": 10000}'
 
 # Populating product data for non-vector search
 if [ ! -f ./icecat-products-w_price-19k-20201127.tar.gz ]; then
@@ -149,51 +111,48 @@ if [ ! -f ./transformed_data.json ]; then
   done
 fi
 echo -e "${MAJOR}Indexing the sample product data, please wait...\n${RESET}"
-curl -u 'elastic:ElasticRocks' -s -X POST "localhost:9200/ecommerce/_bulk?pretty" -H 'Content-Type: application/json' --data-binary @transformed_data.json
+curl -s -X POST "localhost:9200/ecommerce/_bulk?pretty" -H 'Content-Type: application/json' --data-binary @transformed_data.json
 
 
-exit 0
+# echo -e "${MAJOR}Creating default (empty) query rewriters\n${RESET}"
+# # Query rewriters are configured empty, without any rules. This ensures that no errors occur when the different
+# # relevance algorithms are used in the frontend before any rules are set.
+# # For configuring the rules SMUI will be set up and used.
+# curl -s --request PUT 'http://localhost:9200/_querqy/rewriter/common_rules' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "class": "querqy.elasticsearch.rewriter.SimpleCommonRulesRewriterFactory",
+#     "config": {
+#         "rules": ""
+#     }
+# }'
 
+# curl -s --request PUT 'http://localhost:9200/_querqy/rewriter/common_rules_prelive' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "class": "querqy.elasticsearch.rewriter.SimpleCommonRulesRewriterFactory",
+#     "config": {
+#         "rules": ""
+#     }
+# }'
 
-echo -e "${MAJOR}Creating default (empty) query rewriters\n${RESET}"
-# Query rewriters are configured empty, without any rules. This ensures that no errors occur when the different
-# relevance algorithms are used in the frontend before any rules are set.
-# For configuring the rules SMUI will be set up and used.
-curl -u 'elastic:ElasticRocks' -s --request PUT 'http://localhost:9200/_querqy/rewriter/common_rules' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "class": "querqy.elasticsearch.rewriter.SimpleCommonRulesRewriterFactory",
-    "config": {
-        "rules": ""
-    }
-}'
+# curl -s --request PUT 'http://localhost:9200/_querqy/rewriter/replace' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "class": "querqy.elasticsearch.rewriter.ReplaceRewriterFactory",
+#     "config": {
+#         "rules": ""
+#     }
+# }'
 
-curl -u 'elastic:ElasticRocks' -s --request PUT 'http://localhost:9200/_querqy/rewriter/common_rules_prelive' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "class": "querqy.elasticsearch.rewriter.SimpleCommonRulesRewriterFactory",
-    "config": {
-        "rules": ""
-    }
-}'
-
-curl -u 'elastic:ElasticRocks' -s --request PUT 'http://localhost:9200/_querqy/rewriter/replace' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "class": "querqy.elasticsearch.rewriter.ReplaceRewriterFactory",
-    "config": {
-        "rules": ""
-    }
-}'
-
-curl -u 'elastic:ElasticRocks' -s --request PUT 'http://localhost:9200/_querqy/rewriter/replace_prelive' \
---header 'Content-Type: application/json' \
---data-raw '{
-    "class": "querqy.elasticsearch.rewriter.ReplaceRewriterFactory",
-    "config": {
-        "rules": ""
-    }
-}'
+# curl -s --request PUT 'http://localhost:9200/_querqy/rewriter/replace_prelive' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "class": "querqy.elasticsearch.rewriter.ReplaceRewriterFactory",
+#     "config": {
+#         "rules": ""
+#     }
+# }'
 
 echo -e "${MAJOR}Setting up SMUI\n${RESET}"
 while [ $(curl -s http://localhost:9000/api/v1/solr-index | wc -c) -lt 2 ]; do
@@ -203,27 +162,15 @@ done
 curl -X PUT -H "Content-Type: application/json" -d '{"name":"ecommerce", "description":"Chorus Webshop"}' http://localhost:9000/api/v1/solr-index
 
 if $offline_lab; then
-  if $local_deploy; then
-    ./keycloak/check-for-host-configuration.sh
-  fi
-  echo -e "${MINOR}Waiting for Keycloak to be available${RESET}"
-  ./keycloak/wait-for-keycloak.sh
 
   echo -e "${MAJOR}Setting up Quepid${RESET}"
-
   docker compose run --rm quepid bin/rake db:setup
   docker compose run quepid thor user:create -a admin@choruselectronics.com "Chorus Admin" password
 
   echo -e "${MAJOR}Setting up RRE\n${RESET}"
   docker compose run rre mvn rre:evaluate
   docker compose run rre mvn rre-report:report
-fi
 
-if $observability; then
-  echo -e "${MAJOR}Setting up Grafana\n${RESET}"
-  curl -u admin:password -S -X POST -H "Content-Type: application/json" -d '{"email":"admin@choruselectronics.com", "name":"Chorus Admin", "role":"admin", "login":"admin@choruselectronics.com", "password":"password", "theme":"light"}' http://localhost:9091/api/admin/users
-  curl -u admin:password -S -X PUT -H "Content-Type: application/json" -d '{"isGrafanaAdmin": true}' http://localhost:9091/api/admin/users/2/permissions
-  curl -u admin:password -S -X POST -H "Content-Type: application/json" http://localhost:9091/api/users/2/using/1
 fi
 
 echo -e "${MAJOR}Welcome to Chorus ES Edition!${RESET}"
