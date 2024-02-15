86a2a3d2594cc1d1096b77fcf8b7d7a996686d64
jzonthemtn <jzemerick@opensourceconnections.com>
2024-02-15 16:50:08 -0500

    Removing vector stuff from mapping and changing yellow to green.

--- opensearch/wait-for-os.sh
+++ opensearch/wait-for-os.sh
@@ -1,3 +1,3 @@
 DOT='\033[0;37m.\033[0m'
-while [[ "$(curl -s localhost:9200/_cluster/health | jq '."status"')" != "\"yellow\"" ]]; do printf ${DOT}; sleep 5; done
+while [[ "$(curl -s localhost:9200/_cluster/health | jq '."status"')" != "\"green\"" ]]; do printf ${DOT}; sleep 5; done
 echo ""
