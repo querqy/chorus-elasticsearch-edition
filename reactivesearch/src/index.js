67b3a1131838155199847af689c645956daa6b06
RasonJ <145287540+RasonJ@users.noreply.github.com>
2024-02-20 08:28:33 -0800

    Updating interfaces

--- reactivesearch/src/index.js
+++ reactivesearch/src/index.js
@@ -6,40 +6,6 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 
-const sessionResolver = () => cookieSessionResolver();
-
-const queryResolver = () => {
-	const params = new URLSearchParams(window.location.search);
-
-	const query = new Query();
-	query.setSearch(params.get("query"));
-
-	return query;
-}
-const debug = true;
-const trail = new Trail(queryResolver, sessionResolver);
-const context = new Context(window, document);
-const writer = new DemoWriter(queryResolver, sessionResolver, "demo-channel", debug);
-
-const collectorModule = new CollectorModule({
-	writer,
-	context
-});
-
-collectorModule.addLogTransport(new ConsoleTransport());
-
-collectorModule.add(new InstantSearchQueryCollector('[data-track-id="searchBox"]'));
-
-collectorModule.add(new SuggestSearchCollector((writer, type, context) => {
-	new Sentinel(context.getDocument()).on('[data-track-id="suggestSearchTerm"]', (element) => {
-		element.addEventListener("mouseup", () => {
-			writer.write({
-				type,
-				keywords: sanitize(element.textContent)
-			});
-		})
-	});
-}));
 
 
 
