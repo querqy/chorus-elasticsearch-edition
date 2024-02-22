a3a62d2c22f30a37cf3420fa5a60c542d60d87c9
Eric Pugh <epugh@opensourceconnections.com>
2024-02-22 15:20:37 -0500

    workaround script that times out after 60

--- opensearch/wait-for-os.sh
+++ opensearch/wait-for-os.sh
@@ -1,3 +1,18 @@
 DOT='\033[0;37m.\033[0m'
-while [[ "$(curl -s localhost:9200/_cluster/health | jq '."status"')" != "\"green\"" ]]; do printf ${DOT}; sleep 5; done
+start_time=$(date +%s)
+timeout=60   # Some setups return a "yellow" and so this allows us to continue on...
+
+while [[ "$(curl -s localhost:9200/_cluster/health | jq '."status"')" != "\"green\"" ]]; do
+    printf "${DOT}"
+    sleep 5
+
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+
+    if [[ $elapsed_time -ge $timeout ]]; then
+        echo "Timeout reached. Exiting."
+        exit 1
+    fi
+done
+
 echo ""
