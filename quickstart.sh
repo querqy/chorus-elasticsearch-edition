4d925be4904793c87e82b05c358ec9aa636bf9c5
jzonthemtn <jzemerick@opensourceconnections.com>
2024-02-23 08:48:56 -0500

    Increasing max fields for chorus.

--- quickstart.sh
+++ quickstart.sh
@@ -82,7 +82,7 @@ echo -e "${MAJOR}Waiting for OpenSearch to start up and be online.${RESET}"
 
 echo -e "${MAJOR}Creating ecommerce index, defining its mapping & settings\n${RESET}"
 curl -s -X PUT "localhost:9200/ecommerce/" -H 'Content-Type: application/json' --data-binary @./opensearch/schema.json
-curl -s -X PUT "localhost:9200/ecommerce/_settings"  -H 'Content-Type: application/json' -d '{"index.mapping.total_fields.limit": 10000}'
+curl -s -X PUT "localhost:9200/ecommerce/_settings"  -H 'Content-Type: application/json' -d '{"index.mapping.total_fields.limit": 20000}'
 
 # Populating product data for non-vector search
 if [ ! -f ./icecat-products-w_price-19k-20201127.tar.gz ]; then
