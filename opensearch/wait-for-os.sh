ce15aa7efb8b03eee73cc5d2c33cf9b0d1f61de3
Eric Pugh <epugh@opensourceconnections.com>
2024-02-26 13:41:28 -0500

    dont exit and be smarter

--- opensearch/wait-for-os.sh
+++ opensearch/wait-for-os.sh
@@ -2,6 +2,10 @@ DOT='\033[0;37m.\033[0m'
 start_time=$(date +%s)
 timeout=60   # Some setups return a "yellow" and so this allows us to continue on...
 
+# Wait for opensearch to start...
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:9200/_cluster/health)" != "200" ]]; do printf ${DOT}; sleep 5; done
+
+# See if we can get a "green" status...
 while [[ "$(curl -s localhost:9200/_cluster/health | jq '."status"')" != "\"green\"" ]]; do
     printf "${DOT}"
     sleep 5
@@ -11,7 +15,7 @@ while [[ "$(curl -s localhost:9200/_cluster/health | jq '."status"')" != "\"gree
 
     if [[ $elapsed_time -ge $timeout ]]; then
         echo "Timeout waiting for 'green' OpenSearch status reached. Proceeding on."
-        exit 1
+        exit 0
     fi
 done
 
